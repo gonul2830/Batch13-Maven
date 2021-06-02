6952522e60c970a5dd1684c92776633b1e4fcad0
gonul2830 <pirisauruspdf@gmail.com>
2021-06-02 08:00:53 +0300

    C2 Class

--- src/test/java/day07/C2_BeforeAfterAnnotations.java
+++ src/test/java/day07/C2_BeforeAfterAnnotations.java
@@ -11,34 +11,42 @@ import java.util.concurrent.TimeUnit;
 
 public class C2_BeforeAfterAnnotations {
     WebDriver driver;
-    @Before
+@Before
     public void setup(){
         WebDriverManager.chromedriver().setup();
         driver =new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        System.out.println("setup calisti");
+        System.out.println("Setup methodu calıstı");
+
     }
-    @After
-    public void tearDown() throws InterruptedException {
+@After
+    public void tearDown () throws InterruptedException {
         Thread.sleep(5000);
         driver.close();
-        System.out.println("teardown calisti");
+        System.out.println("tearDown methodu calıstı");
+    }
+
+    @Test
+    public void test01() throws InterruptedException {
+
+        driver.get("https://google.com");
+        System.out.println("Test01 methodu calıstı");
+
     }
     @Test
-    public void test01()  {
-        driver.get("https://techproeducation.com");
-        System.out.println("1.metot calisti");
-    }
-    @Test
-    public void test02()  {
+    public void test02() throws InterruptedException {
+
         driver.get("https://www.amazon.com");
-        System.out.println("2.metot calisti");
+        System.out.println("Test02 methodu calıstı");
     }
     @Test
-    public void test03()  {
+    public void test03() throws InterruptedException {
+
         driver.get("https://www.facebook.com");
-        System.out.println("3.metot calisti");
+        System.out.println("Test03 methodu calıstı");
+
     }
+
 
 }
